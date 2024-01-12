65d67f39d6659fc84751b288f3060b18509985a7
rahathasanDev <devrahat.hasan@gmail.com>
2024-01-13 02:05:42 +0600

    routes modification and login page added

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -8,7 +8,7 @@ const auth = getAuth(app);
 
 
 const AuthProvider = ({ children }) => {
-  const user = {displayName:'Muri Khan'}
+  const user = null
   const authInfo = {user}
   return (
     <AuthContext.Provider value={authInfo}>
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Components/Pages/Home";
 import Category from "../Category/Category";
@@ -6,48 +6,65 @@ import Newslayout from "../Layout/Newslayout";
 import News from "../Layout/News";
 import About from "../Layout/About";
 import Career from "../Layout/career";
+import LoginLayout from "../Layout/LoginLayout";
+import Login from "../Layout/Login";
+import Register from "../Layout/Register";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Main></Main>,
-      children: [
-        {
-          path: '/about',
-          element: <About></About>,
-        },
-        {
-          path: '/career',
-          element: <Career></Career>,
-        },
-        {
-          path: '/',
-          element: <Category></Category>,
-          loader:()=>fetch('http://localhost:5000/news') 
-        },
-        {
-          path: '/category/:id',
-          element: <Category></Category>,
-          loader:({params})=>fetch(`http://localhost:5000/categories/${params.id}`)
-        
-        }
-      ]
-    },
-    {
-      path: '/news',
-      element: <Newslayout></Newslayout>,
-      children: [
-        {
-          path: ':id',
-          element: <News></News>,
-          loader:({params})=>fetch(`http://localhost:5000/news/${params.id}`)
-        }
-      ]
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LoginLayout></LoginLayout>,
+    children: [
+      {
+        path: '/',
+        element: <Navigate to="/category/0"></Navigate>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      },
+      {
+        path: 'register',
+        element: <Register></Register>
+      },
+      {
+        path: '/about',
+        element: <About></About>,
+      },
+      {
+        path: '/career',
+        element: <Career></Career>,
+      },
+    ]
 
-    }
+  },
+  {
+    path: '/category',
+    element: <Main></Main>,
+    children: [
+
+      {
+        path: ':id',
+        element: <Category></Category>,
+        loader: ({ params }) => fetch(`http://localhost:5000/categories/${params.id}`)
+
+      }
+    ]
+  },
+  {
+    path: '/news',
+    element: <Newslayout></Newslayout>,
+    children: [
+      {
+        path: ':id',
+        element: <News></News>,
+        loader: ({ params }) => fetch(`http://localhost:5000/news/${params.id}`)
+      }
+    ]
+
+  }
 
 
-  ]
+]
 )
 export default router;
