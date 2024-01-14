8f5a2e06c02d5549411e0f07dced3313430802f9
rahathasanDev <devrahat.hasan@gmail.com>
2024-01-14 19:27:52 +0600

    get input value from login and register form function completed using firebase

--- src/Layout/Login.jsx
+++ src/Layout/Login.jsx
@@ -7,12 +7,26 @@ const Login = () => {
   const {signIn} = useContext(AuthContext);
 
   const handleLogin = event =>{
-    
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.email.value;
+    console.log(name,email,password);
+
+    signIn(email,password)
+    .then(result => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+    })
+    .catch(error => {
+      console.log(error);
+    })
   }
   return (
     <Container className='w-50 mx-auto '>
       <h3>Please Login</h3>
-      <Form className='mt-5'>
+      <Form onSubmit={handleLogin} className='mt-5'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label >Email address</Form.Label>
         <Form.Control type="email" name="email" placeholder="Enter email" required />
